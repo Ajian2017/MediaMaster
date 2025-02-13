1938439f82eed741147b94586130076ab9a3960b
Amob <qzjian@gmail.com>
2025-02-13 11:24:31 +0800

    Initial Commit

---
+++ MediaMaster/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MediaMaster
+//
+//  Created by Amob on 2025/2/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MediaMaster/MediaMasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  MediaMasterApp.swift
+//  MediaMaster
+//
+//  Created by Amob on 2025/2/13.
+//
+
+import SwiftUI
+
+@main
+struct MediaMasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
